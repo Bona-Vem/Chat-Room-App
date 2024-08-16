5f61c97decad389b4c366d29e5aa5f7752665778
abhinavarya <abhinavarya330@gmail.com>
2024-08-16 16:09:33 +0530

    Multiple User Chatting App (SpringBoot + WebSocket)

---
+++ src/main/java/com/chat/chatroomapp/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.chat.chatroomapp.controller;
+
+import com.chat.chatroomapp.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message){
+//        try{
+//            Thread.sleep(1000);
+//        }catch (InterruptedException ex){
+//            ex.printStackTrace();
+//        }
+        return message;
+    }
+}
